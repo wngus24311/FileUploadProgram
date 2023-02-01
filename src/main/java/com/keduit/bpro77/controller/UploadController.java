183b5f7d8c0fd04f338e32c6e5d53046dab9e04f
wngus24311 <wngus24311@gmail.com>
2023-02-01 15:16:51 +0900

    File Upload program

--- src/main/java/com/keduit/bpro77/controller/UploadController.java
+++ src/main/java/com/keduit/bpro77/controller/UploadController.java
@@ -1,19 +1,82 @@
 package com.keduit.bpro77.controller;
 
+import com.keduit.bpro77.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @Log4j2
 public class UploadController {
+	@Value("{com.keduit.upload.path}")
+	private String uploadPath;
+	
 	@PostMapping("/uploadAjax")
-	public void uploadFile(MultipartFile[] uploadFiles) {
+	public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles) throws Exception {
+		
+		List<UploadResultDTO> resultDTOList = new ArrayList<>();
 		for (MultipartFile uploadFile : uploadFiles) {
+			if (!uploadFile.getContentType().startsWith("image")) {
+				log.warn("이미지 파일을 넣어주세요");
+				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+			}
 			String originalName = uploadFile.getOriginalFilename();
 			String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
 			log.info("FileName========>" + fileName);
+			String folderPath = makeFolder();
+			String uuid = UUID.randomUUID().toString();
+			String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+			Path savePath = Paths.get(saveName);
+			uploadFile.transferTo(savePath);
+			resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
 		}
+		return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+	}
+	
+	private String makeFolder() {
+		String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+		String folderPath = str.replace("//", File.separator);
+		File uploadPathFolder = new File(uploadPath, folderPath);
+		if (!uploadPathFolder.exists()) {
+			uploadPathFolder.mkdirs();
+		}
+		return folderPath;
+	}
+	
+	@GetMapping("/display")
+	public ResponseEntity<byte[]> getFile(String fileName) {
+		ResponseEntity<byte[]> result = null;
+		try {
+			String srcFileName = URLDecoder.decode(fileName, StandardCharsets.UTF_8);
+			log.info("fileName : " + fileName);
+			File file = new File(uploadPath + File.separator + srcFileName);
+			HttpHeaders header = new HttpHeaders();
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return result;
 	}
 }
---
+++ src/main/java/com/keduit/bpro77/dto/UploadResultDTO.java
@@ -0,0 +1,26 @@
+package com.keduit.bpro77.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Data
+@AllArgsConstructor
+public class UploadResultDTO {
+	private String fileName;
+	private String uuid;
+	private String folderPath;
+	
+	public String getImageURL() {
+		try {
+			return URLEncoder.encode(folderPath + "/" + uuid + "_" + fileName, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return "";
+	}
+	
+}
