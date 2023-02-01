bd16e4e36a7b57a95df3c9cc5c2a78ff3add30c9
wngus24311 <wngus24311@gmail.com>
2023-02-01 12:20:41 +0900

    File Upload program

---
+++ src/main/java/com/keduit/bpro77/controller/UploadTestController.java
@@ -0,0 +1,12 @@
+package com.keduit.bpro77.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UploadTestController {
+	@GetMapping("/uploadEx")
+	public void uploadEx() {
+	
+	}
+}
